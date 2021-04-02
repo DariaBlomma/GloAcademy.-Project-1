ba0c7d40c3653fecacf66d6c39ea0aab6c665f33
Daria Kozina <kozina.flowers@yandex.ru>
2021-04-02 14:39:20 +0300

    HW 1

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,1 +1,12 @@
 'use strict';
+let money,
+    income, 
+    addExpenses,
+    deposit, 
+    mission, 
+    period;
+
+
+alert('If today was your last day and tomorrow was too late ...');
+
+console.log('Created undefined variables :' + money, income, addExpenses, deposit, mission, period); 
