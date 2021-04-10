060f73b5ef6653a21fdc0cb13b4ca0cd3baa4c1d
Daria Kozina <kozina.flowers@yandex.ru>
2021-04-10 21:22:38 +0300

    Hw 7 hard

--- Project 1/Hard tasks/script.js
+++ Project 1/Hard tasks/script.js
@@ -1,35 +1,18 @@
 'use strict';
 //hard task
-let num = 266219;
-let numString = num.toString(); 
-console.log(' numString: ',  numString);
+let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let currDay = new Date().getDay();
+console.log(currDay);
+console.log(week[6]);
+let ul = document.createElement('ul');
+document.body.prepend(ul);
 
-let result = 1;
+week.reverse().map((item, index) => {
+    if(item === 'Sunday' || item === 'Saturday') {
+        ul.insertAdjacentHTML('afterbegin', `<li class='cursive'>${item}</li>`);
+    } else {
+        ul.insertAdjacentHTML('afterbegin', `<li>${item}</li>`);
+    }
+});
 
-for (let i = 0; i < numString.length; i++) {
-    result = result * (numString[i] * numString[++i]);
-}
-
-//variant 2
-
-let arrString = numString.split('');
-console.log('arrString: ', arrString);
-
-let mapNum = arrString.map(function (item) {
-    return Number(item);
-    });
-console.log('mapNum: ', mapNum);
-
-let multiple = arrString.reduce(function (mult, current) {
-    
-    return mult * current;
-}, 1);
-console.log('multiple: ', multiple); //1296
-
-
-
-console.log(result); //1296
-
-result = result ** 3; //2176782336
-
-console.log(result.toString().slice(0, 2)); //21
+ul.children[currDay].classList.add('bold');
