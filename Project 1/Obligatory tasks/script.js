5c251c8465b8e0ff5f71e0595b4fbbefe725aef4
Daria Kozina <kozina.flowers@yandex.ru>
2021-04-11 22:59:15 +0300

    Hw 8 obligatory

--- Project 1/Obligatory tasks/script.js
+++ Project 1/Obligatory tasks/script.js
@@ -9,34 +9,57 @@ let isNumber = function(n) {
 let money,
     start = function() {//repeat asking while not a number
         do {
-            money = prompt('Ваш месячный доход?');//month
+            money = prompt('Ваш месячный доход?', '5000');//month
         } while (!isNumber(money));
     };
 
     start();
-
+// правильная сумма для проверки - при сумме расходов 300, бюджет на день 156
 let appData = {
     income: {},
     addIncome: [],
     expenses: {},
     addExpenses: [],
     deposit: false,
+    percentDeposit: 0,
+    moneyDeposit: 0,
     mission: 12000,
     period: 12,
     asking: function() {
+        if(confirm(' Есть ли у Вас дополнительный источник заработка?')) {
+            let itemIncome, cashIncome ='';
+            // пропускает деление на 0. Непорядок
+            do {
+                itemIncome = prompt('Какой у Вас дополнительный заработок?','Таксую');
+            } while (isNumber(itemIncome));
+
+            
+            do {
+                cashIncome = prompt(' Сколько в месяц Вы на этом зарабатываете?', '1000');
+            } while (!isNumber(cashIncome));
+            
+            appData.income[itemIncome] = cashIncome;
+        }
         let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Sambo, laptop, lead');
-            appData.addExpenses = addExpenses.toLowerCase().split(', ');
+            // \b matches a word boundary (the beginning or ending of word);
+            // \w matches the following meta-character [a-zA-Z0-9].
+            // g is global search, not only first appearance
+            appData.addExpenses = addExpenses.toLowerCase().replace(/\b\w/g, l => l.toUpperCase()).split(', ');
             appData.deposit = confirm('Есть ли у вас депозит в банке?');
         
         let expenses = [];
         for (let i = 0; i < 2; i++) {
             //узнали расходы и записали в объект
-            expenses[i] = prompt('Введите обязательную статью расходов?', 'food');
-            let amount = prompt('Во сколько обойдется ?');
-
-            if(isNumber(amount)) {
-                appData.expenses[expenses[i]] = +amount;
-            }
+            do {
+                expenses[i] = prompt('Введите обязательную статью расходов?', 'food');
+            } while (isNumber(expenses[i]));
+            
+            let amount;
+            do {
+                amount = prompt('Во сколько обойдется ?', '100');
+            } while (!isNumber(amount));
+            
+            appData.expenses[expenses[i]] = +amount;
         }
     },
     budget: money,
@@ -70,10 +93,23 @@ let appData = {
             return 'negative number' + appData.budgetDay;
         }
     },
+    getInfoDeposit: function() {
+        if(appData.deposit) {
+            do {
+                appData.percentDeposit = prompt(' Какой годовой процент?', '10');
+            } while (!isNumber(appData.percentDeposit));
+            do {
+                appData.moneyDeposit = prompt(' Какая сумма заложена?', '10000');
+            } while (!isNumber(appData.moneyDeposit));
+
+        }
+    },
+    calcSavedMoney: function() {
+        return appData.budgetMonth * appData.period;
+    }
 };
 //Delete these logs?
 //console.log(addExpenses.length);
-//let income = +prompt('Дополнительные доходы', 2300);
 
 
 //console.log(`The period is ${appData.period} months`);
@@ -99,3 +135,7 @@ console.log('Наша программа включает в себя данны
 for (let key in appData) {
     console.log(key, appData[key]);
 }
+
+appData.getInfoDeposit();
+
+console.log('appData.addExpenses: ', appData.addExpenses.join(', '));
