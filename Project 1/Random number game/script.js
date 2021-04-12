845f5a822640f7e8f09a153cc8056b0f2a20f64d
Daria Kozina <kozina.flowers@yandex.ru>
2021-04-12 19:29:35 +0300

    Example solution from Max Leskin

--- Project 1/Random number game/script.js
+++ Project 1/Random number game/script.js
@@ -1,7 +1,7 @@
 // //game random number
 'use strict';
 
-let isNumber = function(n) {
+/*let isNumber = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
@@ -60,4 +60,81 @@ const startGame= function() {
 };
 
 startGame();
+*/
+// решение Маскима Лескина
+const getRandomInt = function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    // min = Math.min(min, max);
+    // max = Math.max(min, max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
+const numCheck = function(n) {
+    return !isNaN(parseInt(n)) && isFinite(n);
+};
+
+const getNumber = function(str) {
+    const numUser = prompt(str);
+
+    if(numUser === null) return null;
+    if(numCheck(numUser)) {
+        return +numUser;
+    }
+
+    alert(' Кажется, вы забыли ввести число. Попробуйте еще раз');
+    getNumber(str);
+};
+
+
+const getCounter = function() {
+    let counter = 0;
+    return function() {
+        return counter++;
+    };
+};
+
+
+const gameBot = function(attempts, min, max) {
+    attempts = attempts || 10; // если забыли передать кол-во попыток, то по умолчанию 10
+    min = min || 0;
+    max = max || 100;
+    const random = getRandomInt(min, max);
+    const counter =  getCounter();
+
+    return function checkNumber() {
+        
+
+        const count = counter();
+        if(count < attempts) {
+            const number = getNumber(' Попробуйте угадать число от ' + min + ' до ' + max);
+            if (number === null) return alert(' До свидос Амиго! ');
+
+            if (number > random) {
+            alert(' Загаданное число меньше чем Ваше');
+            return checkNumber();
+            }
+            if(number < random) {
+                alert(' Загаданное число больше');
+                return checkNumber();
+            }
+            if(number === random) {
+                alert(' Молодец! Угадал!');
+            } 
+        } else {
+            alert(' Кол-во попыток закончилось. Было загадано число ' + random);
+        }
+
+        const questAC = confirm(' Хотите сыграть заново? ');
+        if(questAC) {
+            alert(' Отлично! Начинаем!');
+            gameBot(attempts, min, max)();
+        } else {
+            alert(' Спасибо за игру! Еще увидимся');
+        }
+        
+    };
+    
+};
+
+gameBot(10, 1, 10)();
