649fb04a7601a03f9968608eb750878cbb402203
Daria Kozina <kozina.flowers@yandex.ru>
2021-04-21 23:29:41 +0300

    have prompts check, show users

--- Project 1/authorization/script.js
+++ Project 1/authorization/script.js
@@ -10,10 +10,7 @@ let user,
     userPassword;
 
 let checkPromps = function (field) {
-    if (field === null) {
-        alert('Its okay that you dont want to sign up on this strange site. Bye!');
-        return false;
-    }
+    return field === null || field.trim() === '';
 };
 
 // const isText = /([A-Za-zа-яА-Я]+\s([A-Za-zа-яА-Я]+\s|[A-Za-zа-яА-Я]+))/;
@@ -33,30 +30,54 @@ let userNameCheck = function () {
 // matches any word with only 1 space and any word with or without space. Two words are required
 let usersInfo = [];
 authorize.addEventListener('click', function() {
-    do {
-        user = prompt(' Write your name and surname');
-    } while (user.trim() === '');
-
     // if (user === null) {
     //     alert ('Its okay that you dont want to sign up on this strange site. Bye!');
     // }
 
+    do {
+        user = prompt(' Write your name and surname');
+    } while (checkPromps(user));
+
     userNameCheck();
+    const userArr = user.split(' '),
+        userName = userArr[0],
+        userSurname = userArr[1],
+        date = new Date(),
+        currDay = date.getDate(),
+        currMonth = date.getMonth(),
+        currYear = date.getFullYear(),
+        currTime = date.toLocaleTimeString(),
+        monthsRu = [' января', ' февраля ', ' марта', ' апреля', ' мая ', ' июня', ' июля', ' августа', ' сентября', ' октября', ' ноября', ' декабря'],
+            changeEnding = function(date, text1, text2, text3) {
+            if (String(date).endsWith('1')) {
+                p.innerHTML += ` ${text1} `;
+            } else if (String(date).endsWith('2') || String(date).endsWith('3') || String(date).endsWith('4')) {
+                p.innerHTML += ` ${text2} `;
+            } else {
+                p.innerHTML += ` ${text3} `;
+            }
+        };
+        // let currMonth= new Date().getMonth();
+        // p.innerHTML += monthsRu[currMonth];
+        // p2.innerHTML += addZero(++currMonth, '.');
+        const currDate = `${currDay} ${monthsRu[currMonth]} ${currYear} ${currTime}`;
+    showUsers(userName, userSurname, currDate);
+    do {
+        userLogin = prompt('Set your login');
+    } while (checkPromps(userLogin));
+
+    do {
+        userPassword = prompt('Set your password');
+    } while (checkPromps(userPassword));
 
     
     // console.log('user: ', user);
     
-    // let userArr = user.split(' ');
-    // let userName = userArr[0];
-    // let userSurname = userArr[1];
 
-    // do {
-    //     userLogin = prompt('Set your login');
-    // } while (!checkPromps(userLogin));
 
-    // do {
-    //     userPassword = prompt('Set your password');
-    // } while (!checkPromps(userPassword));
+
+
+
 
     // let userObj = {
         
@@ -70,3 +91,10 @@ authorize.addEventListener('click', function() {
     // console.log('usersInfo: ', usersInfo);
     
 });
+
+const showUsers = function (name, surname, date) {
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    li.textContent = `Name: ${name}, Surname: ${surname}, Sign up date: ${date}`;
+    ul.append(li);
+};
