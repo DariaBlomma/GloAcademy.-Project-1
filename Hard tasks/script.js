c26adc0d5bc7d96562aa51e47acb1c94de137d63
Daria Kozina <kozina.flowers@yandex.ru>
2021-04-22 21:09:47 +0300

    HW17 hard. Task 3 - selects

--- Hard tasks/script.js
+++ Hard tasks/script.js
@@ -1,13 +1,45 @@
 'use strict';
-function getResult(x,y){
-    let result;
-    // ваш код
-    let degree =  String(x ** y);
-    result = degree.split('');
-    result = result.reduce((acc, item) => {
-        return acc + Number(item);
-    }, 0);
-    return result
+const cityArr = {
+    rus: ['Москва', 'Санк-Петербург', 'Новосибирск', 'Екатеринбург', 'Нижний Новгород', 'Казань', 'Челябинск'],
+    uk: ['Киев', 'Харьков', 'Одесса', 'Днепр', 'Донецк', 'Запорожье', 'Львов'],
+    bel: ['Минск', 'Гомель', 'Могилёв', 'Витебск', 'Гродно', 'Брест'],
+    jap: ['Токио', 'Киото', 'Осака', 'Иокогама'] 
 }
 
-console.log(getResult(4, 8))
+
+const countrySelect = document.querySelector('#country'),
+        citySelect = document.querySelector('#city'),
+        result = document.querySelector('.result');
+
+const fillSelect = elem => {
+    let cityOption = document.createElement('option');
+    cityOption.textContent = elem;
+    citySelect.append(cityOption);
+    citySelect.style.display = 'inline-block';
+};
+
+const clearSelect = () => {
+    const options = document.querySelectorAll('#city option');
+    options.forEach(item => item.remove())
+};
+
+const bind = (opt) => {
+    for (const key in cityArr) {
+        if (opt === key) {
+            const arr = cityArr[key].map(item => {
+                fillSelect(item);
+            });
+        }
+    }
+};
+
+countrySelect.addEventListener('change', () => {
+    clearSelect();
+    const option = countrySelect.value;
+    bind (option);
+});
+
+citySelect.addEventListener('change', () => {
+    const option = citySelect.value;
+    result.textContent = countrySelect.options[countrySelect.selectedIndex].innerHTML + ' ' + option;
+})
