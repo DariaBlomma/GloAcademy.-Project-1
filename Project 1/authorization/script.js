6e0dad64e22dc729a29169f6ff87d5da95bb9f95
Daria Kozina <kozina.flowers@yandex.ru>
2021-04-23 23:16:31 +0300

    authorization

--- Project 1/authorization/script.js
+++ Project 1/authorization/script.js
@@ -1,41 +1,32 @@
 'use strict';
-let span = document.querySelector('.user-name');
-let authorize = document.querySelector('.authorize');
-let logIn = document.querySelector('.log-in');
-
-
-let user,
+let span = document.querySelector('.user-name'),
+    authorize = document.querySelector('.authorize'),
+    logIn = document.querySelector('.log-in'),
+    user,
     userArr,
     userLogin,
-    userPassword;
+    userPassword,
+    // usersInfo = [];
+    usersInfo = new Map();
 
-let checkPromps = function (field) {
+const checkPromps = field => {
     return field === null || field.trim() === '';
 };
+
 
 // const isText = /([A-Za-zа-яА-Я]+\s([A-Za-zа-яА-Я]+\s|[A-Za-zа-яА-Я]+))/;
 // const check = new RegExp('([A-Za-zа-яА-Я]+\s([A-Za-zа-яА-Я]+\s|[A-Za-zа-яА-Я]+))', '');
 // should match any word with only 1 space and any word with or without space. Two words are required
 const isText2 = /\D/g;
 
-let userNameCheck = function () {
+const userNameCheck = () => {
     userArr =  user.split(' ');
-    
     while (userArr.length !== 2 || !isText2.test(user)) {
         alert ('This is unappropriate format. Please write exactly 2 words and do not use numbers or other signs');
         user = prompt(' Write your name and surname');
         userArr =  user.split(' ');
     }
 };
-
-let usersInfo = [];
-const parsed = JSON.parse(localStorage.getItem('usersInfo'));
-if (parsed) {
-    parsed.forEach(item => {
-        console.log('lc exists');
-        showUsers(item.name, item.surname, item.date);
-    });
-}
 
 const saveUsers = (name, surname, login, password, date) => {
     let userObj = {
@@ -46,19 +37,72 @@ const saveUsers = (name, surname, login, password, date) => {
         'date': date,
     };
 
-    usersInfo.push(userObj);
-    localStorage.setItem('usersInfo', JSON.stringify(usersInfo));
+    //usersInfo.push(userObj);
+    usersInfo.set(name, userObj);
 };
 
-const showUsers = function (name, surname, date) {
+const lcPush = () => {
+    for (let [key, value] of usersInfo) {
+        localStorage.setItem(`${key}`, JSON.stringify(value));
+    }
+}
+const renderBtns = item => {
+    const btn = document.createElement('button');
+    btn.textContent = 'Delete user';
+    btn.style.marginLeft = '10px';
+    btn.className = 'delete';
+    item.append(btn);
+};
+
+const showUsers = (name, surname, date) => {
     const ul = document.querySelector('ul');
     const li = document.createElement('li');
-    li.textContent = `Name: ${name}, Surname: ${surname}, Sign up date: ${date}`;
+    li.style.paddingBottom = '10px';
+    li.innerHTML = `<b>Name:</b> ${name}, <b>Surname:</b> ${surname}, <b>Sign up date:</b> ${date}`;
     ul.append(li);
+    renderBtns(li);
+};
+
+for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    if (key) {
+        console.log('lc exists');
+        const parsed = JSON.parse(localStorage.getItem(`${key}`));
+        showUsers(parsed.name, parsed.surname, parsed.date);
+        saveUsers(parsed.name, parsed.surname, parsed.login, parsed.password, parsed.date);
+    }
+}
+
+const ul = document.querySelector('ul');
+
+
+const deleteUser = index => {
+    const btns = ul.querySelectorAll('.delete');
+    for (let i = 0; i < btns.length; i++) {
+        if (i === index) {
+            const li = ul.children[i].textContent;
+            // delete usersInfo[index];
+            ul.children[i].remove();
+            const pos1 = li.indexOf(':') + 1;
+            const pos2 = li.indexOf(',');
+            const key = li.substring(pos1, pos2).trim();
+            usersInfo.delete(`${key}`);        
+            localStorage.removeItem(`${key}`);
+        }
+    }
 };
 
 
-authorize.addEventListener('click', function() {
+ul.addEventListener('click', event => {  
+    const btns = ul.querySelectorAll('.delete');  
+    btns.forEach((elem, index) => {
+        if (event.target === elem) {
+            deleteUser(index);
+        }
+    })
+});
+
+authorize.addEventListener('click', () => {
     do {
         user = prompt(' Write your name and surname');
     } while (checkPromps(user));
@@ -72,16 +116,7 @@ authorize.addEventListener('click', function() {
         currMonth = date.getMonth(),
         currYear = date.getFullYear(),
         currTime = date.toLocaleTimeString(),
-        monthsRu = [' января', ' февраля ', ' марта', ' апреля', ' мая ', ' июня', ' июля', ' августа', ' сентября', ' октября', ' ноября', ' декабря'],
-            changeEnding = function(date, text1, text2, text3) {
-            if (String(date).endsWith('1')) {
-                p.innerHTML += ` ${text1} `;
-            } else if (String(date).endsWith('2') || String(date).endsWith('3') || String(date).endsWith('4')) {
-                p.innerHTML += ` ${text2} `;
-            } else {
-                p.innerHTML += ` ${text3} `;
-            }
-        };
+        monthsRu = [' января', ' февраля ', ' марта', ' апреля', ' мая ', ' июня', ' июля', ' августа', ' сентября', ' октября', ' ноября', ' декабря'];
 
     const currDate = `${currDay} ${monthsRu[currMonth]} ${currYear} ${currTime}`;
     showUsers(userName, userSurname, currDate);
@@ -94,20 +129,35 @@ authorize.addEventListener('click', function() {
         userPassword = prompt('Set your password');
     } while (checkPromps(userPassword));
 
-    saveUsers(userName, userSurname, userLogin, userPassword, currDate);
-    // let userObj = {
-    //     'name': `${userName}`,
-    //     'surname': `${userSurname}`,
-    //     'login': `${userLogin}`,
-    //     'password': `${userPassword}`,
-    // };
-
-    // usersInfo.push(userObj);
-    // console.log('usersInfo: ', usersInfo);
-    // localStorage.setItem('usersInfo', JSON.stringify(usersInfo));
+    saveUsers(userName, userSurname, userLogin, userPassword, currDate, userNumber);
+    lcPush();
 });
 
 
+logIn.addEventListener('click', () => {
+    let logLogin,
+        logPassword;
+    do {
+        logLogin = prompt('Write your login');
+    } while (checkPromps(logLogin));
+
+    do {
+        logPassword = prompt('Write your password');
+    } while (checkPromps(logPassword));
+
+    logLogin = logLogin.trim();
+    logPassword = logPassword.trim();
+    if (usersInfo.has(logLogin)) {
+        if (logPassword === usersInfo.get(logLogin).password) {
+            span.style.color = 'blue';
+            span.textContent = logLogin;
+        } else {
+            alert('This is the wrong password');
+        }
+    } else {
+        alert('No such user found');
+    }
+});
 
 
 
