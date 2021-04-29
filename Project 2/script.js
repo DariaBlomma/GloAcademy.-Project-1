0882cf34a949441838288821add1079d52da5204
Daria Kozina <kozina.flowers@yandex.ru>
2021-04-29 21:58:48 +0300

    HW24. Calculator

--- Project 2/script.js
+++ Project 2/script.js
@@ -448,7 +448,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     changePhotos();
 
-    //calculator
+    //validation
     const validation = () => {
         const numbers = event => {
             const target = event.target;
@@ -513,5 +513,53 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     validation();
+
+    //calculator
+    const calculator = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcCount = document.querySelector('.calc-count'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            totalValue = document.getElementById('total');
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        };
+        calcBlock.addEventListener('change', event => {
+            const target = event.target;
+            // if (target.matches('.calc-count') || target.matches('.calc-square') ||
+            //  target.matches('.calc-day') || target.matches('.calc-type')) {
+            //      console.log('works');
+            //  }
+
+            // if (target === calcType || target === calcSquare ||
+            // target === calcDay || target === calcCount) {
+            //     console.log('works');
+            // }
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+        });
+    };
+
+    calculator(100);
 
 });
